d11165b6017d3d156a263e4573bf4e0165a4238f
Nessa Jane Marin <nessajane@nessajane.tech>
2025-09-14 14:13:57 -0700

    Moving to Bluefin
    Bluefin has some key advantages changing my dotfiles:
    1. Using Linux brew... so I can simplify installation when switching
    between OSX and my Linux machines.
    2. Immutability, container tooling... I'm going to iterate on this
    setup and figure out how to use distrobox, devpod etc to aim for
    one command setup of my dev environment across operating systems.
    
    I used to have that setup and it was insanely complex. It looks
    like the tooling around containerization may make it easy now.

--- install.sh
+++ install.sh
@@ -1,14 +1,22 @@
 chsh -s /usr/bin/zsh
 
-echo "Installing some stuff..."
-sudo pacman -S neovim
-
-# ruby-build pre-reqs
-sudo pacman -S --needed base-devel rust libffi libyaml openssl
-
-# Install rbenv
-git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-~/.rbenv/bin/rbenv init
+echo "[1/4] Installing LazyVim"
+brew install neovim
 
 # LazyVim
 git clone http://github.com/LazyVim/starter ~/.config/nvim
+
+echo "[2/4] Installing oh-my-post"
+# oh-my-post
+brew install jandedobbeleer/oh-my-posh/oh-my-posh
+oh-my-posh font install hack
+
+echo "[3/4] Copying zsh config"
+cp ./zsh/.exports ~/.exports
+cp ./zsh/.aliases ~/.aliases
+echo 'source ~/.exports' >> ~/.zshrc
+echo 'source ~/.aliases' >> ~/.zshrc
+
+echo "[4/4] Copying git config"
+cp ./git/.gitignore ~/.gitignore
+cp ./git/.gitconfig ~/.gitconfig
