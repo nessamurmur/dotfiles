7ce23b2b79fd65107a6426a89a99c667a6be3443
Nessa Jane Marin <nessajane@nessajane.tech>
2025-09-14 14:18:33 -0700

    Add warp to install script

--- install.sh
+++ install.sh
@@ -1,22 +1,25 @@
 chsh -s /usr/bin/zsh
 
-echo "[1/4] Installing LazyVim"
+echo "[1/5] Installing LazyVim"
 brew install neovim
 
 # LazyVim
 git clone http://github.com/LazyVim/starter ~/.config/nvim
 
-echo "[2/4] Installing oh-my-post"
+echo "[2/5] Installing oh-my-post"
 # oh-my-post
 brew install jandedobbeleer/oh-my-posh/oh-my-posh
 oh-my-posh font install hack
 
-echo "[3/4] Copying zsh config"
+echo "[3/5] Copying zsh config"
 cp ./zsh/.exports ~/.exports
 cp ./zsh/.aliases ~/.aliases
 echo 'source ~/.exports' >> ~/.zshrc
 echo 'source ~/.aliases' >> ~/.zshrc
 
-echo "[4/4] Copying git config"
+echo "[4/5] Copying git config"
 cp ./git/.gitignore ~/.gitignore
 cp ./git/.gitconfig ~/.gitconfig
+
+echo "[5/5] Installing warp"
+brew install --cask warp
